c100cc89156c4e8b4f6ae444d624dead13002e3d
Vanessa <vsochat@stanford.edu>
2014-08-16 12:29:58 -0700

    First Commit adding Core Module and script to run it

---
+++ authorSynth.py
@@ -0,0 +1,102 @@
+# authorSynth: This script produces a brain map that represents
+# the brain spatial coverage published by a particular author.
+# It includes functions to query pubmed for an author of interest,
+# and then crosslist the doi's of those articles with NeuroSynth
+
+# 1) query pubmed for an author of interest, and returns dois 
+# and orders
+# 2) cross list dois with those in the neuroSynth database
+# 3) produce a brainmap for the list of PMIDs 
+
+__author__ = "Vanessa Sochat, Peter Lee"
+__copyright__ = "Copyright 2014"
+__credits__ = ["Vanessa Sochat", "Peter Lee"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Vanessa Sochat"
+__email__ = "vsochat@stanford.edu"
+__status__ = "Development"
+
+import numpy as np
+from neurosynth.base.dataset import Dataset
+from neurosynth.base.dataset import FeatureTable
+from neurosynth.base import imageutils
+from neurosynth.analysis import meta
+import nibabel as nb
+from nibabel import nifti1
+from Bio import Entrez
+
+# -- NEUROSYNTH FUNCTIONS --------------------------------------------------------------
+
+def neurosynthInit(dbsize):
+    """Initialize Neurosynth Database, return database object"""
+    print "Initializing Neurosynth database..."
+    dataset = Dataset('data/' + str(dbsize) + 'terms/database.txt')
+    dataset.add_features('data/' + str(dbsize) + 'terms/features.txt')    
+
+    #print "Loading standard space brain..."
+    #img = nb.load("data/MNI152_T1_2mm_brain.nii.gz")
+    #standard = img.get_data()
+    return dataset
+
+def neurosynthMatch(papers):
+    """Match neurosynth doi with papers doi"""
+    dois = papers.keys()
+    # NOT YET WRITTEN
+    #TODO - 1) get list of dois from neuroSynth database
+    #       2) return meta analysis with those dois!
+    #       3) write function to output text / image result
+    # https://github.com/neurosynth/neurosynth/blob/master/neurosynth/analysis/meta.py
+    # https://github.com/neurosynth/neurosynth/blob/master/neurosynth/base/dataset.py
+
+def getFeatures(dataset):
+    """Return features in neurosynth database"""
+    return dataset.get_feature_names()
+
+# -- PUBMED FUNCTIONS --------------------------------------------------------------
+# These functions will find papers of interest to crosslist with Neurosynth
+
+def getArticles(author,email):
+    """Return dictinary of dois and order based on author name (Last FM)"""
+
+    print "Getting pubmed articles for author " + searchTerm
+    
+    Entrez.email = email
+    handle = Entrez.esearch(db='pubmed',term=author,retmax=5000)
+    record = Entrez.read(handle)
+
+      # If there are papers
+      if "IdList" in record:
+        if record["Count"] != "0":
+          # Fetch the papers
+          ids = record['IdList']
+          handle = Entrez.efetch(db='pubmed', id=ids,retmode='xml',retmax=5000)
+          records = Entrez.read(handle)
+          papers = dict()
+          for record in records:
+            authors = record["MedlineCitation"]["Article"]["AuthorList"]
+            order = 1
+            for p in authors:
+              # If it's a collective, won't have a last name
+              if "LastName" in p:
+                person = p["LastName"] + " " + p["Initials"]
+                if person == author:
+                  # Only really old papers won't have a doi
+                  for r in record["PubmedData"]["ArticleIdList"]:
+                    if bool(re.search("[/]",str(r))):
+                      # If they are last, they are PI
+                      if order == len(authors):
+                        papers[str(r)] = "PI"
+                      else:
+                        papers[str(r)] = order
+              order = order + 1
+
+        # If there are no papers
+        else:
+          print "No papers found for author " + author + "!"
+          
+    # Return dictionary of dois and author order
+    return papers
+
+if __name__ == "__main__":
+  print "Please import as a module"
---
+++ run_authorSynth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# import authorSynth module
+import authorSynth as AS
+
+# Init Neurosynth database, either "525" or "3000" terms
+db = AS.neurosynthInit("525")
+
+# Query pubmed for author of interest
+# will return dictionary of PMIDs and author order
+author = "Hariri AR"
+email = "vsochat@gmail.com"
+papers = AS.getArticles(author,email)
+
+# NOT YET WRITTEN
+someresult = AS.neurosynthMatch(papers)
+# output a result? input arg to specify output?
