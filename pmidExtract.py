5342ac0f75614e82e6db2ebb86ddafc48ce83517
bhellion <spambin_10@hotmail.com>
2014-08-19 00:18:18 -0700

    modified: pmidExtract.py
    return a unique set of pmids

--- pmidExtract.py
+++ pmidExtract.py
@@ -4,16 +4,16 @@
 # import authorSynth module
 import authorSynth as AS
 
-db = AS.neurosynthInit("525")
+def idExtract(dbsize):
 
-neurosynth_ids = db.image_table.ids
+	db = AS.neurosynthInit(dbsize)
+	neurosynth_ids = db.image_table.ids
+	ns_ids_set = set(neurosynth_ids)
 
-if bool(re.search("[/]",neurosynth_ids[0])):
-  print "Search for " + str(len(dois)) + " ids in NeuroSynth database..."
-  # Find intersection
-  valid_ids = [x for x in dois if x in neurosynth_ids]
-else:
-  print "Search for " + str(len(pmid)) + " ids in NeuroSynth database..."
-  valid_ids = [str(x) for x in pmid if str(x) in neurosynth_ids]
-print "Found " + str(len(valid_ids)) + "."
-
+	if bool(re.search("[/]",neurosynth_ids[0])):
+	  print "Processing conversion of dois to pmid"
+	  # Some code to convert dois to pmid using http://www.pubmedcentral.nih.gov/utils/idconv/v1.0/?ids=10.1093/nar/gks1195
+	else:
+	  print "Extracted pmids"
+	  
+	return ns_ids_set
